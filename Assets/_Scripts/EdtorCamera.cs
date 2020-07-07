1567fb23ebde4cfd655f612249a1ffe57310fa3a
Ioannis Boutsikas <mgioanni@outlook.com>
2020-07-07 12:35:54 -0400

    New project layout. Add tools to be able to navigate in editor only

---
+++ Assets/_Scripts/EdtorCamera.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EdtorCamera : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float HorizontalSensitivity = 1.0f;
+    public float VerticalSensitivity = 1.0f;
+    public float MovementSpeed = 10.0f;
+    public bool FlipY = true;
+    public bool FlipX = false;
+
+    [Header("Keybinds")]
+    public KeyCode Forwards = KeyCode.W;
+    public KeyCode Backwards = KeyCode.S;
+    public KeyCode Left = KeyCode.A;
+    public KeyCode Right = KeyCode.D;
+    public KeyCode Up = KeyCode.Space;
+    public KeyCode Down = KeyCode.X;
+    public KeyCode CameraLock = KeyCode.Mouse2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(CameraLock))
+        {
+            DoMouseInput();
+        }
+        DoKeyboardInput();
+    }
+
+    void DoKeyboardInput()
+    {
+        Vector3 translation = Vector3.zero;
+
+        if (Input.GetKey(Forwards))
+        {
+            translation += transform.forward* MovementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(Backwards))
+        {
+            translation -= transform.forward * MovementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(Left))
+        {
+            translation -= transform.right * MovementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(Right))
+        { 
+            translation += transform.right * MovementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(Up))
+        {
+            translation += transform.up * MovementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(Down))
+        {
+            translation -= transform.up * MovementSpeed * Time.deltaTime;
+        }
+
+        transform.Translate(translation);
+    }
+
+
+    void DoMouseInput()
+    {
+
+        float horizontalRotation = HorizontalSensitivity * Input.GetAxis("Mouse X");
+        float verticalRotation = VerticalSensitivity * Input.GetAxis("Mouse Y");
+
+        if (FlipY)
+        {
+            horizontalRotation *= -1.0f;
+        }
+
+        transform.Rotate(Vector3.up, horizontalRotation, Space.World);
+
+        float rotationAngle = Mathf.Clamp(verticalRotation, -85.0f, 85.0f);
+        if (FlipX)
+        {
+            rotationAngle *= -1.0f;
+        }
+        transform.Rotate(transform.right, rotationAngle, Space.World);
+    }
+}
---
+++ Assets/_Scripts/ParticleAroundCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleAroundCamera : MonoBehaviour
+{
+    public Camera target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.transform.position;
+    }
+}
